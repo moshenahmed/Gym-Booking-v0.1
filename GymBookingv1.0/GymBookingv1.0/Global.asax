6f4d730c26bd9427b36b4e55832c0f9b9c53b291
mosh <moshenahmed@gmail.com>
2018-01-02 09:43:29 +0100

    on doing

---
+++ GymBookingv1.0/GymBookingv1.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GymBookingv1._0.MvcApplication" Language="C#" %>
---
+++ GymBookingv1.0/GymBookingv1.0/Models/GymClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GymBookingv1._0.Models
+{
+    public class GymClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime StartTime { get; set; }
+        [Required]
+        [DataType(DataType.Duration)]
+        public TimeSpan Duration { get; set; }
+        public DateTime EndTime => StartTime + Duration;
+        public string Description { get; set; }
+        public virtual ICollection<ApplicationUser> AttendingMembers { get; set; }
+    }
+}
